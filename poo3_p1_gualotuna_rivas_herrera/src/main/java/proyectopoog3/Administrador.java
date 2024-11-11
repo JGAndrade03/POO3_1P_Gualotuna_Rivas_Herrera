87f58bd3bfdbdae5c917c367dd7be15b3f1d2037
HansRivas <hanrivas@espol.edu.ec>
2024-11-10 21:28:21 -0500

    Clase Administrador, App, MA y Rezerva

---
+++ poo3_p1_gualotuna_rivas_herrera/src/main/java/proyectopoog3/Administrador.java
@@ -0,0 +1,5 @@
+package proyectopoog3;
+
+public class Administrador {
+    
+}
--- poo3_p1_gualotuna_rivas_herrera/src/main/java/proyectopoog3/App.java
+++ poo3_p1_gualotuna_rivas_herrera/src/main/java/proyectopoog3/App.java
@@ -1,6 +1,6 @@
 package proyectopoog3;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
